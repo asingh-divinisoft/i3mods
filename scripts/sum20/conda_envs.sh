1bcade80cc210d64cf75a6cb773ded54c22a3ce1
Aaditya Singh <asaaditya8@gmail.com>
2022-01-04 00:54:59 +0530

    removed apt install jupyter-notebook

--- scripts/sum20/conda_envs.sh
+++ scripts/sum20/conda_envs.sh
@@ -2,6 +2,6 @@ conda create -n jupy -c conda-forge jupyterlab jupytext -y
 conda create -n web -c conda-forge python ipython ipykernel beautifulsoup4 selenium requests tqdm pandas -y
 
 rustup component add rust-src
-sudo apt install jupyter-notebook cmake build-essential
+#sudo apt install jupyter-notebook cmake build-essential
 cargo install evcxr_jupyter
 evcxr_jupyter --install
